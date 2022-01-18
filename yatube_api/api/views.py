d40f343cba022709d252826533941a323fccdd64
Evgenii Terskikh <eterskikh@nvidia.com>
2022-01-18 19:11:08 +0300

    4-th take

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -7,7 +7,7 @@ from rest_framework import mixins
 from .permissions import AuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
-from posts.models import Group, Post, User
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
